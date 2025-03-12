09a16581e41da17b5e3e135df9f04438f950e9bc
Hamza Simsek <hamzas4011@hotmail.com>
2025-03-12 17:49:13 +0100

    update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
-import Navbar from "../components/Navbar";
-import "../styles/globals.css";
+import "@/styles/globals.css";
+import Navbar from "@/components/Navbar";
+
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
