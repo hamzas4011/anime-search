fd6356557ec6e953de9ded7f0f5b2a47406924a6
Hamza Simsek <hamzas4011@hotmail.com>
2025-03-12 18:07:36 +0100

    update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="text-center">
-      <h1 className="text-4xl font-bold my-6">Welcome to Anime Exøprer</h1>
+      <h1 className="text-4xl font-bold my-6">Welcome to Anime Explorer</h1>
       <p className="text-lg">Find and explore your favorite anime!</p>
     </div>
   );
