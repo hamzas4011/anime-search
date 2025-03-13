bc0f27f471c2c616bd3c4525f7b13d88611b02c1
Hamza Simsek <hamzas4011@hotmail.com>
2025-03-13 22:53:30 +0100

    update footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,9 @@ import Footer from "@/components/Footer";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="bg-gray-100 text-gray-900">
+      <body className="bg-gray-100 text-gray-900 min-h-screen flex flex-col">
         <Navbar />
-        <main className="container mx-auto p-4">{children}</main>
+        <main className="container mx-auto p-4 flex-grow">{children}</main>
         <Footer />
       </body>
     </html>
