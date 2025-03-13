0c2406c2bab5faa6bee6ec59eb58883e905026b3
Hamza Simsek <hamzas4011@hotmail.com>
2025-03-13 22:45:51 +0100

    update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,13 @@ import "@/styles/globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className="bg-gray-100 text-gray-900">
         <Navbar />
         <main className="container mx-auto p-4">{children}</main>
+        <Footer />
       </body>
     </html>
   );
