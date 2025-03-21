5b60180af2a84d19f87e0a4421b3537545d2f18a
Hamza Simsek <hamzas4011@hotmail.com>
2025-03-21 16:28:26 +0100

    update route

--- src/app/api/popular/route.tsx
+++ src/app/api/popular/route.tsx
@@ -1,11 +1,10 @@
 export async function GET() {
-    try {
-      const response = await fetch("https://api.jikan.moe/v4/top/anime");
-      const data = await response.json();
-  
-      return Response.json(data.data);
-    } catch (error) {
-      return Response.json({ error: "Failed to fetch popular anime" }, { status: 500 });
-    }
+  try {
+    const response = await fetch("https://api.jikan.moe/v4/top/anime");
+    const data = await response.json();
+
+    return Response.json(data.data.slice(0, 16)); // Limit to 16 animes
+  } catch (error) {
+    return Response.json({ error: "Failed to fetch anime." }, { status: 500 });
   }
-  
+}
