4413d3d016c4408ea3c07c34e79d746c0fb2c429
Hamza Simsek <hamzas4011@hotmail.com>
2026-08-12 19:09:35 +0200

    d

--- src/app/lib/jikan.ts
+++ src/app/lib/jikan.ts
@@ -15,7 +15,7 @@ type JikanAnime = {
 let cachedAnime: JikanAnime[] | null = null;
 
 async function fetchOnce(): Promise<JikanAnime[]> {
-  const response = await fetch("https://api.jikan.moe/v4/top/anime?type=ona", {
+  const response = await fetch("https://api.jikan.moe/v4/seasons/now?sfw", {
     next: { revalidate: 21600 },
   });
 
